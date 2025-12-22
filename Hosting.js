5bb20cdc52143b80ae8d2d73662ea766bf513c7d
Akthar24 <165126033+Akthar24@users.noreply.github.com>
2025-12-22 16:27:42 +0530

    Add files via upload

---
+++ Hosting.js
@@ -0,0 +1,26 @@
+//Hosting
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// var a = 10;// undefined
+// let b = 20; //error
+// const c = 30; //error
+
+
+//Before printing the function can call above the function whereas let,const cannot ,it makes error
+//function is hosting
+
+
+add(2, 3);
+
+function add(a, b) {
+  console.log(a+b);
+}
+
+text()
+function text(){
+  console.log(10);
+}
