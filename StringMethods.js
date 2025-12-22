fefadff51fcad717ff09ea9905289b0d24f17b5c
Akthar24 <165126033+Akthar24@users.noreply.github.com>
2025-12-22 16:25:40 +0530

    Add files via upload

---
+++ StringMethods.js
@@ -0,0 +1,39 @@
+// let text = " Hello World ";
+// //length
+// console.log(text.length);
+// //toUpperCase()
+// console.log(text.toUpperCase());
+// //toLowerCase()
+// console.log(text.toLowerCase());
+// //charAt(index)
+// console.log(text.charAt(6));
+// //indexOf
+// console.log(text.indexOf("World"));
+// //lastIndexOf
+// console.log(text.lastIndexOf("d"));
+// //includes(value)-Check whether the String contains a value
+// console.log(text.includes("World"));
+// //startsWith()
+// console.log(text.startsWith("Hello"));
+// //endsWith()
+// console.log(text.endsWith("World"));
+// //slice()-Negative values are also allowed
+// console.log(text.slice(0,4));
+// //substring(startIndex,endIndex)
+// console.log(text.substring(4,10))
+// //replace(old,new)
+// console.log(text.replace("World","Welcome"));
+// //replaceAll(old,new)
+// console.log(text.replaceAll("Hello","Hello!"));
+// //trim()- trimStart(),trimEnd()
+// console.log(text.trim());
+// //split()-Seperator
+// let result=text.split(",");
+// console.log(result);
+// //concat()
+// let text1="Welcome";
+// console.log(text.concat(" ",text1));
+// //repeat()
+// console.log(text.repeat(3));
+// //search()
+// console.log(text.search("world"));
